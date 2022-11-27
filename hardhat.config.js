e5fde78320e7ad02687b25ff63a03987bfe2c443
syskey <lopetvers@gmail.com>
2022-11-27 11:37:22 +0000

    first

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: '0.8.17',
+  networks: {
+    goerli: {
+      url: process.env.QUICKNODE_API_KEY_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "AAK5VWA9PEBP3PERV2H2VP3XMIRYNNKMRS",
+  }
+};
